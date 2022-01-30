3a94c0d17548189b9f9fb874d78dbe5fe690f75b
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-01-30 17:33:37 +0330

    add pdf

---
+++ ex2/ex2.js
@@ -0,0 +1,33 @@
+function timeoutPromiseResolve(interval) {
+    return new Promise((resolve, reject) => {
+      setTimeout(function(){
+        resolve("successful");
+      }, interval);
+    });
+  };
+  
+  function timeoutPromiseReject(interval) {
+    return new Promise((resolve, reject) => {
+      setTimeout(function(){
+        reject("error");
+      }, interval);
+    });
+  };
+  
+  async function timeTest() {
+    const timeoutPromiseResolve1 = timeoutPromiseResolve(5000);
+    const timeoutPromiseReject2 = timeoutPromiseReject(2000);
+    const timeoutPromiseResolve3 = timeoutPromiseResolve(3000);
+  
+    const res = await Promise.all([timeoutPromiseResolve1, timeoutPromiseReject2, timeoutPromiseResolve3]);
+    return res;
+  }
+  
+  let startTime = Date.now();
+  timeTest().then(() => {
+  }).catch(e => {
+    console.log(e);
+    let finishTime = Date.now();
+    let timeTaken = finishTime - startTime;
+    alert("Time taken in milliseconds: " + timeTaken);
+  })
